80c019226f5323ff4749635ae75c6d243ad9bb29
João Veiga <jvdvandrade1996@gmail.com>
2026-01-02 20:59:02 -0300

    Executing media directly from user documents, still no media control.

--- video/video/views/playerView.xaml.cs
+++ video/video/views/playerView.xaml.cs
@@ -1,27 +1,80 @@
-﻿using System.Text;
+﻿using System;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using System.IO;
+using System.Windows.Threading;
+using System.Text;
+using Microsoft.Win32;
 
 namespace video.Views
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for playerView.xaml
     /// </summary>
-
     public partial class PlayerView : Window
     {
+
+        // STATE
+
+        private bool isPLaying = false;
+        private bool IsUserDraggingSlider = false;
+
+        // SERVICES / HELPERS
+
+        private readonly DispatcherTimer Timer = new()
+        {
+            Interval = TimeSpan.FromSeconds(0.1)
+        };
+
+        private readonly OpenFileDialog MediaOpenDialog = new()
+        {
+            Title = "Open a media file",
+            Filter = "Media Files (*.mp3,*.mp4)|*.mp3;*.mp4"
+        };
+
+        // CONSTRUCTOR
 
         public PlayerView()
         {
             InitializeComponent();
-        }
-    }
 
+            Timer.Tick += Timer_Tick;
+            Timer.Start();
+        }
+
+        // UI EVENTS
+        private void TestBtn_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Clicked here!");
+        }
+
+        private void OpenBtn_click(object sender, RoutedEventArgs e)
+        {
+            if (MediaOpenDialog.ShowDialog() == true)
+            {
+                Player.Source = new Uri(MediaOpenDialog.FileName);
+                //TitleLbl.Content = Path.GetFileName(MediaOpenDialog.FileName);
+
+                Player.Play();
+                isPLaying = true;
+            }
+        }
+
+        // TIMER / PLAYER SYNC
+
+        private void Timer_Tick(object? sender, EventArgs e)
+        {
+            if (Player.Source != null &&
+                Player.NaturalDuration.HasTimeSpan &&
+                !IsUserDraggingSlider)
+            {
+                ProgressSlider.Maximum =
+                    Player.NaturalDuration.TimeSpan.TotalSeconds;
+
+                ProgressSlider.Value =
+                    Player.Position.TotalSeconds;
+            }
+        }
+
+
+    }
 }
