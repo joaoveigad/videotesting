6b582c03b3b6820bb20bf2e054a12dee1fffec17
João Veiga <jvdvandrade1996@gmail.com>
2026-01-10 21:35:43 -0300

    '

--- video/video/viewmodels/PlayerViewModel.cs
+++ video/video/viewmodels/PlayerViewModel.cs
@@ -2,7 +2,6 @@
 using System.Collections.ObjectModel;
 using System.Windows;
 using System.Windows.Input;
-using System.Windows.Media;
 using System.Windows.Threading;
 using video.models;
 using video.services.Interfaces;
